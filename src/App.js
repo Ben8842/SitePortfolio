ec93b960cb0735a84866157e0db904710a45da7e
Ben8842 <z87e99b44r55a@gmail.com>
2021-02-23 12:47:08 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+var firstimg = require("./imgfolder/pattern.jpg").default;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id="patternBack" src={firstimg} alt="mystery"></img>
+      <div>
+        <div id="about">
+          <p>Name: Benjamin Perkins</p>
+          <p>GitHub: https://github.com/Ben8842</p>
+          <p>Contact: perkinsben@yahoo.com</p>
+          <p>Status: currently searching for employment</p>
+          <p> Skills: </p>
+          <p>• React, Redux, JavaScript, Python, Java, HTML, CSS, XML</p>
+          <p>
+            • MongoDB, SQL, Mongoose, Express, • GitHub, Heroku, Netlify, Visual
+            Studio{" "}
+          </p>
+          <p>• FIX Protocol, PowerShell, FTP, TFS, JIRA</p>
+
+          <div id="fixy">
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://tender-benz-d42ce4.netlify.app/">
+                Eight Queens Puzzle
+              </a>
+            </button>
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://jolly-meninsky-2047bc.netlify.app/">
+                Pascal's Triangle and the Sierpinsky Triangle
+              </a>
+            </button>
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://optimistic-babbage-2ed085.netlify.app/">
+                Maze Algorithm
+              </a>
+            </button>
+            <button class="fancy" variant="primary" size="lg" block>
+              <a href="https://trivia-redux.herokuapp.com/">Trivia</a>
+            </button>
+
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://vibrant-spence-207a4e.netlify.app/">
+                Emojii Matching Game
+              </a>
+            </button>
+
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://benwperkins.github.io/BitProject/">
+                Cryptocurrency Project
+              </a>
+            </button>
+
+            <button class="fancy" variant="secondary" size="lg" block>
+              <a href="https://wonderful-mahavira-3cec2e.netlify.app/">
+                Trivia Application Tutorial
+              </a>
+            </button>
+            <button class="fancy" variant="primary" size="lg" block>
+              <a href="https://wordsearch-generator.herokuapp.com/">
+                Word Search Generator
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
